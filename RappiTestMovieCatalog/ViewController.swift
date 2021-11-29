34ccdf3f20472189130e2c70c536f3e3206a5a7f
Adrian Dominguez <adriandgzg@gmail.com>
2021-11-29 15:09:10 -0600

    Initial Commit

---
+++ RappiTestMovieCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RappiTestMovieCatalog
+//
+//  Created by Adrian Dominguez Gómez on 29/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

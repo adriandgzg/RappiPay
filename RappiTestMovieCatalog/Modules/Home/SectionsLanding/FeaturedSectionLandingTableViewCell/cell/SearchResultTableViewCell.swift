7b091f7ed1214205c03a9d2a4031cf90946d8def
Adrian Dominguez <adriandgzg@gmail.com>
2021-12-14 00:54:25 -0600

    add searchBar and Update Ofline mode

---
+++ RappiTestMovieCatalog/Modules/Home/SectionsLanding/FeaturedSectionLandingTableViewCell/cell/SearchResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultTableViewCell.swift
+//  RappiTestMovie
+//
+//  Created by Adrian Dominguez Gómez on 14/12/21.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
